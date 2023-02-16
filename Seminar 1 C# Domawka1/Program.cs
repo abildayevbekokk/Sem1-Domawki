31228cfad80583b48477673dcde505548cb5d16c
Bekzhan <abildayevbekokk@gmail.com>
2023-02-16 21:28:36 +0600

    Pervaya domawka

---
+++ Seminar 1 C# Domawka1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+
+if (a>b)
+    Console.WriteLine("Ваше максимальное число = " + a);
+else if (b<a)
+    Console.WriteLine("Ваше максимальное число = " + b);
+else 
+    Console.WriteLine("Они равны");
+
+
--- Seminar1 C# Task 5/Program.cs
+++ Seminar1 C# Task 5/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+if (a / 100 == 0 || a / 100 > 9)
+Console.WriteLine("Число не является трехзначным");
+else
 Console.WriteLine(a%10);
