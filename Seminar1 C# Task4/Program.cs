0812f0a277ef48175178c37c789fda41f3b1b967
Bekzhan <abildayevbekokk@gmail.com>
2023-02-16 20:51:25 +0600

    Chetyre zadachi

---
+++ Seminar1 C# Task4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = -a; i<=a; i++)
+    Console.Write(i + " ");
