9d8138fb2218a7c237b6dbf19ea44cb23e7eecd4
Bekzhan <abildayevbekokk@gmail.com>
2023-02-16 20:33:24 +0600

    Dve zadachi est

---
+++ Seminar 1 C# Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+string input1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string input2 = Console.ReadLine();
+int a = Convert.ToInt32(input1);
+int b = Convert.ToInt32(input2);
+
+if (a*a == b)
+    Console.WriteLine("Одно число является квадратом другого");
+else if (b*b == a)
+    Console.WriteLine("Одно число является квадратом другого");
+else 
+    Console.WriteLine("Ни одно число не является квадратом другого");
+
